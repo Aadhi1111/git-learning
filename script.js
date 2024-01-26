61eea1dd19bd69f4302dd73cc19ee652184a05e2
Aadhi1111 <aadhishmoorthy@gmai[Cl.com>
2024-01-26 20:14:06 +0530

    changes of two files

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-var request = new XMLHttpRequest();
+//XML-HTTP Methods:
+//Purpose : it is used to interact with the
+//servers via API
 
-request.open("GET","http://restcountries.com/v3.1/all")
+//Total there are 5 steps.
+
+//Step 01: Create a XML-HTTP Object
+var request = new XMLHttpRequest();
+//Step 02: Open a Request
+request.open("GET","https://restcountries.com/v3.1/all");
+//Step 03: Kickstart a request
+request.send();
+//step 04:
+//Once the data successfully recived from the server
+//server status code is 200
+//data coming from the server is of type string
+//convert string to object
+
+request.onload = function(){
+var res = JSON.parse(request.response); 
+for(i=0;i<res.length;i++){
+    if(res[i].capital){
+        console.log(res[i].capital[0] +" "+res[i].area);
+    }
+    else{
+        console.log("Invalid Capital"+" "+res[i].area);
+    }
+    
+}
+}
